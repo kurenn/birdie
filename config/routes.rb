07cb709c7913c7f9e9818007143c879219792d9a
Abraham Kuri <kurenn@icalialabs.com>
2021-05-30 16:33:46 -0500

    Updates routes for pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/about'
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/home',     to: 'pages#home'
+  get '/about',    to: 'pages#about'
+  get '/contact',  to: 'pages#contact'
+  get '/faq',      to: 'pages#faq'
 end
