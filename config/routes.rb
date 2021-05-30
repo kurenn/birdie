0c6bbcc9c12ee72a551c605f94b446ff3b8a420d
Abraham Kuri <kurenn@icalialabs.com>
2021-05-30 16:42:06 -0500

    Updates the root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get '/home',     to: 'pages#home'
   get '/about',    to: 'pages#about'
   get '/contact',  to: 'pages#contact'
   get '/faq',      to: 'pages#faq'
+
+  root to: 'pages#home'
 end
