5257562d493f1365c7d40244202a0b66df3b54b0
Abraham <kurenn@icalialabs.com>
2021-05-30 16:43:38 -0500

    Pages controller lesson (#1)
    * Creates a pages controller with home and about
    
    * Changes permissions for generated files
    
    * Adds contact and faq views
    
    * Updates routes for pages controller
    
    * Adds title instance variable for each action on pages controller
    
    * Adds title tag using content for
    
    * Updates the root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/about',    to: 'pages#about'
+  get '/contact',  to: 'pages#contact'
+  get '/faq',      to: 'pages#faq'
+
+  root to: 'pages#home'
 end
